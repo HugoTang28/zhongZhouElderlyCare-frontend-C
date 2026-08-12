5630ae63f1721642c2e37cf0e14fb75db1fe95ee
tangxujia <2913223934@qq.com>
2026-08-12 22:01:36 +0800

    feat: h5编译

--- dist/dev/mp-weixin/app.js
+++ dist/dev/mp-weixin/app.js
@@ -10,10 +10,14 @@ if (!Math) {
   "./pages/messages/messages.js";
 }
 const _sfc_main = {
-  __name: "App",
-  setup(__props) {
-    return () => {
-    };
+  onLaunch() {
+    console.log("App Launch");
+  },
+  onShow() {
+    console.log("App Show");
+  },
+  onHide() {
+    console.log("App Hide");
   }
 };
 const zzPlaceholder = () => "./components/zz-placeholder/zz-placeholder.js";
